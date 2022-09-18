9d9e15d3dff03a658b1834c15262f280a885df0e
Fan <73092326+Fan-He@users.noreply.github.com>
2022-09-17 20:39:16 -0400

    Added hazard and restart scene
    Added the functional hazard, the player will die immediately if collide with the hazard, and the level will restart.

---
+++ Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+
+    //private Animator anim;
+
+    private void Start()
+    {
+        //anim = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Hazard"))
+        {
+            Die();
+        }
+        Debug.Log("hit");
+    }
+
+    private void Die()
+    {
+        //anim.SetTrigger("death");
+
+        //diable player movement
+        GetComponent<Player>().enabled = false;
+        Debug.Log("died");
+        RestartLevel();
+    }
+
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
