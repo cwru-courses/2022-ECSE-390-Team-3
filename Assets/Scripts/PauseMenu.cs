c9099cabe8250f0525d3e4d3a6ffa58f88a6c516
kasperlatawiec <kasperkite@gmail.com>
2022-09-28 12:47:30 -0400

    Squashed commit of the following:
    commit 7eac62a38ed8f82a1c27fc2a8e85f8222d5d64b7
    Author: kasperlatawiec <kasperkite@gmail.com>
    Date:   Mon Sep 26 19:54:10 2022 -0400
    
        Controls Temporarily Replaces Settings
    
    commit 6329366275ecb14a80c1c248833f5f7fe2312f23
    Author: kasperlatawiec <kasperkite@gmail.com>
    Date:   Thu Sep 22 22:21:12 2022 -0400
    
        Quick Fixes
    
    commit e89123a834765febc161768a3732213acec3bd22
    Author: kasperlatawiec <kasperkite@gmail.com>
    Date:   Thu Sep 22 22:07:41 2022 -0400
    
        Cutscene
    
    commit bcd3fb174185f90104729ae7de19941775266e90
    Author: kasperlatawiec <kasperkite@gmail.com>
    Date:   Thu Sep 22 18:26:53 2022 -0400
    
        Working Pause Menu
    
    commit 715c5e4bfb9441808b9978dfe4b62f0578cecd90
    Author: kasperlatawiec <kasperkite@gmail.com>
    Date:   Thu Sep 22 16:45:09 2022 -0400
    
        Small Update
    
    commit 6c0890cbdd9319919fe0c0498dac3176cd47bb6e
    Author: kasperlatawiec <kasperkite@gmail.com>
    Date:   Thu Sep 22 14:04:38 2022 -0400
    
        Updated Title Screen, begin Pause Screen

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GamePaused = false;
+    public GameObject PauseMenuUI;
+    public GameObject ConfirmMenuUI;
+    public GameObject OptionsMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        ConfirmMenuUI.SetActive(false);
+        OptionsMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+    }
+
+    public void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        ConfirmMenuUI.SetActive(false);
+        OptionsMenuUI.SetActive(false);
+        Time.timeScale = 0f;
+        GamePaused = true;
+    }
+
+    public void Confirm()
+    {
+        PauseMenuUI.SetActive(false);
+        ConfirmMenuUI.SetActive(true);
+        OptionsMenuUI.SetActive(false);
+    }
+
+    public void ConfirmYes()
+    {
+        PauseMenuUI.SetActive(false);
+        ConfirmMenuUI.SetActive(false);
+        OptionsMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+        SceneController.LoadSpecificScene("Main Menu");
+    }
+
+    public void ConfirmNo()
+    {
+        OptionsMenuUI.SetActive(false);
+        ConfirmMenuUI.SetActive(false);
+        PauseMenuUI.SetActive(true);
+    }
+
+    public void Options()
+    {
+        PauseMenuUI.SetActive(false);
+        OptionsMenuUI.SetActive(true);
+        PauseMenuUI.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+     public float delay = 0;
+     public string NextScene= "Main Menu";
+
+     public void Start()
+     {
+          if(delay > 0) 
+          {
+          StartCoroutine(LoadSceneWithDelay(delay));
+          }
+     }
+     
+     public void LoadScene()
+     {
+          SceneManager.LoadScene(NextScene);
+     }
+
+    public static void LoadSpecificScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+        IEnumerator LoadSceneWithDelay(float delay)
+     {
+          yield return new WaitForSeconds(delay);
+          SceneManager.LoadScene(NextScene);
+     }
+
+     public void Quit()
+     {
+          Application.Quit();
+     }
+}
