b9557bc2fa6cea82221fb92f7c62dc7a41c028a3
LiamGongola <59128783+LiamGongola@users.noreply.github.com>
2022-10-12 21:38:23 -0400

    added projectile enemy prefab and bullet sprite

--- Assets/Scripts/EnemyProjectile.cs
+++ Assets/Scripts/EnemyProjectile.cs
@@ -10,27 +10,36 @@ public class EnemyProjectile : MonoBehaviour
     float nextFire;
     public float range = 10f;
     GameObject target;
+    public Animator animator;
 
 
     void Start()
     {
         nextFire = Time.time;
         target = GameObject.FindGameObjectWithTag("Player");
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Vector3.Distance(target.transform.position, transform.position) <= range)
+        Vector3 scale = new Vector3(1.5f, 1.5f, 1);
+        transform.localScale = scale;
+        animator.SetBool("inRange", false);
+
+        if (Vector3.Distance(target.transform.position, transform.position) <= range)
         {
             CheckIfTimeToFire();
+            animator.SetBool("inRange", true);
         }
     }
 
     void CheckIfTimeToFire()
     {
-        if(Time.time > nextFire)
+        if (Time.time > nextFire)
         {
+
             Instantiate(bullet, transform.position, Quaternion.identity);
             nextFire = Time.time + fireTime;
         }
