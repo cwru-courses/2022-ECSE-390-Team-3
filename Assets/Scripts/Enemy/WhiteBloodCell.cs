057adfc18aafcc7b39d917b1d13367212ba3da97
LiamGongola <59128783+LiamGongola@users.noreply.github.com>
2022-12-04 21:23:33 -0500

    boss appearance

--- Assets/Scripts/Enemy/WhiteBloodCell.cs
+++ Assets/Scripts/Enemy/WhiteBloodCell.cs
@@ -14,21 +14,22 @@ public class WhiteBloodCell : MonoBehaviour
     public Material spriteDefault;
     public GameObject door;
     public GameObject bossDripPrefab;
-    public GameObject[] organs;
     Camera cam;
     private float bossHitPauseTime = 0.125f;
+    public bool notFinal;
     private CameraBehavior CB;
-    public bool isFinal;
+    public GameObject transition;
+    public GameObject angry;
     public AudioManager AM;
+
 
 
     // Start is called before the first frame update
     void Start()
-    { 
-        AM = FindObjectOfType<AudioManager>();
+    {
         cam = Camera.main;
-        CB = cam.GetComponent<CameraBehavior>();
         gameManager = GameObject.Find("GameManager");
+        CB = cam.GetComponent<CameraBehavior>();
         //pointIndex = patrolPoints.Length - 2;
     }
 
@@ -39,7 +40,8 @@ public class WhiteBloodCell : MonoBehaviour
 
         if (bonked && anim.GetCurrentAnimatorStateInfo(0).IsName("wbc swim"))
         {
-            swim();
+            if (notFinal) StartCoroutine(swimDelay(1f));
+            else swim();
         }
 
         if (anim.GetCurrentAnimatorStateInfo(0).IsName("white blood cell idle"))
@@ -62,48 +64,46 @@ public class WhiteBloodCell : MonoBehaviour
 
         if (collision.gameObject.tag == "Player" && anim.GetCurrentAnimatorStateInfo(0).IsName("white blood cell idle"))
         {
-            if(AM != null){
+            if (AM != null)
+            {
                 AM.Play("bonk");
             }
-            harmed();
+            hit();
 
             if (patrolPoints.Length - 1 == pointIndex)
             {
                 dead();
+
             }
             else
             {
-                if (isFinal)
-                {
-                    StartCoroutine(rotateDelay(0.7f));
-                    
-                }
+                if (!notFinal) StartCoroutine(rotateDelay(0.7f));
                 else
                 {
-                    Debug.Log("rgrg");
                     CB.stopCam = true;
-                    //unfreezeCamera(3f);
+                    gameManager.GetComponent<GameManager>().OnKeyGet(null, null);
+                    if (transition != null) transition.transform.position = new Vector3(transition.transform.position.x, transition.transform.position.y, 0);
+                    Instantiate(angry, new Vector3(this.transform.position.x + 2, this.transform.position.y + 4, this.transform.position.z + 1), Quaternion.identity);
+                    StartCoroutine(moveTube(1.8f));
+                    StartCoroutine(removeAngry(0.75f));
 
                 }
 
                 if (pointIndex < patrolPoints.Length - 1) pointIndex++;
+                // Debug.Log(pointIndex);
                 bonked = true;
 
             }
 
-            if (pointIndex == 1) FindObjectOfType<Player>().bonkedBoss(new Vector2(0, 200));
-            else if (pointIndex == 2) FindObjectOfType<Player>().bonkedBoss(new Vector2(200, 0));
-            else if (pointIndex == 3) FindObjectOfType<Player>().bonkedBoss(new Vector2(0, -200));
-            else if (pointIndex == 4) FindObjectOfType<Player>().bonkedBoss(new Vector2(-200, 0));
-            else if (pointIndex == 5) FindObjectOfType<Player>().bonkedBoss(new Vector2(0, -200));
+            FindObjectOfType<Player>().bonkedBoss(new Vector2(0, 200));
             collision.gameObject.GetComponentInParent<Player>();
 
         }
 
     }
-
-    void harmed()
+    void hit()
     {
+
         sr.material = flash;
         StartCoroutine(flashDelay(0.2f));
         anim.SetBool("bonked", true);
@@ -115,7 +115,6 @@ public class WhiteBloodCell : MonoBehaviour
         CB.stopCam = true;
         anim.SetBool("dead", true);
         StartCoroutine(bossDrip(2.75f));
-        StartCoroutine(unfreezeCamera(5f));
         gameManager.GetComponent<GameManager>().OnKeyGet(null, door);
     }
 
@@ -163,32 +162,38 @@ public class WhiteBloodCell : MonoBehaviour
     {
         yield return new WaitForSeconds(time);
         sr.material = spriteDefault;
-
     }
 
     IEnumerator rotateDelay(float time)
     {
         yield return new WaitForSeconds(time);
         gameManager.GetComponent<GameManager>().Rotate90();
-
     }
 
     IEnumerator bossDrip(float time)
     {
         yield return new WaitForSeconds(time);
         Instantiate(bossDripPrefab, new Vector3(this.transform.position.x, this.transform.position.y - 5, this.transform.position.z), Quaternion.identity);
-        GetComponent<Renderer>().enabled = false;
-        Instantiate(organs[0], this.transform.position, Quaternion.identity);
-        Instantiate(organs[1], this.transform.position, Quaternion.identity);
-        Instantiate(organs[2], this.transform.position, Quaternion.identity);
-      
     }
 
-    IEnumerator unfreezeCamera(float time)
+    IEnumerator moveTube(float time)
     {
         yield return new WaitForSeconds(time);
-        CB.stopCam = false;
-    
-    }
+        transition.transform.position = new Vector3(transition.transform.position.x, transition.transform.position.y, -1);
     }
 
+    IEnumerator swimDelay(float time)
+    {
+        yield return new WaitForSeconds(time);
+        swim();
+    }
+
+    IEnumerator removeAngry(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(GameObject.FindWithTag("Angry"));
+    }
+
+
+
+}
